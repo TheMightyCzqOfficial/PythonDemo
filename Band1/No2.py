492789082fcf2165d5362a143b20caea46372ff4
xjf <shaweng@21cn.com>
2020-09-23 11:41:29 +0800

    0921

--- Band1/No2.py
+++ Band1/No2.py
@@ -5,7 +5,7 @@ def test_regex():
     res = requests.get("https://www.csdn.net")
     info = res.content.decode("utf-8")
     #print(info)
-    result = re.findall("<img src=\".*\" ", info)
+    result = re.findall("<img src=\"(.*?)\"", info)
     for item in result:
         print(item)
 
