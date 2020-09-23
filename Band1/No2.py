4cad6938671be3278759648fc41ddb021db07732
xjf <shaweng@21cn.com>
2020-09-23 11:12:41 +0800

    0921

---
+++ Band1/No2.py
@@ -0,0 +1,15 @@
+import re
+import requests
+
+def test_regex():
+    res = requests.get("https://www.csdn.net")
+    info = res.content.decode("utf-8")
+    #print(info)
+    result = re.findall("<img src=\".*\" ", info)
+    for item in result:
+        print(item)
+
+
+
+if __name__ == "__main__":
+    test_regex()
